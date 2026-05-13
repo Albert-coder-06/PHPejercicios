f220d218197a4c3eb078cf6f2fa3b2c4b76f159f
Albert <albdanvic@alu.edu.gva>
2026-05-13 20:17:13 +0200

    Me rindo

--- Ejercicio18/ValidadorParentesis.php
+++ Ejercicio18/ValidadorParentesis.php
@@ -1,8 +1,11 @@
 <?php
 
-    $parentesis = "{[]{}}{}";
+    $parentesis = "{[][}{]}";
+    $parentesisPrueba1 = "{{}}";    
 
     function validarParentesis(string $value) {
+
+
         $parejasParentesis = [
             "(" => ")",
             "{" => "}",
@@ -14,17 +17,71 @@
             //first level char
             $char1 = $value[$w];
 
-            //
+            if (!isset($parejasParentesis[$char1])) {
+                continue;
+            }
+
+            //Quantity of levels of the same bracket
             $levelQuantity = 1;
-            for($w2 = $w + 1; $w <= strlen($value) - 1; $w2++) {
-                if ($value[$w2] === $parejasParentesis[$char1]) {
+            $firstLevelIsClosed = false;
+
+            for($w2 = $w + 1; $w2 <= strlen($value) - 1; $w2++) {
+                $char2 = $value[$w2];
+
+                if ($char2 === $char1) {
                     $levelQuantity++;
+                } else if ($char2 === $parejasParentesis[$char1] && $levelQuantity === 1) {
+                    $firstLevelIsClosed = true;
+                    break;
+                } else if ($char2 === $parejasParentesis[$char1]) {
+                    $levelQuantity--;
                 }
+            }
+
+            if ($levelQuantity !== 1 || $levelQuantity === 1 && !$firstLevelIsClosed) {
+                return false;
             }
 
         }
         return true;
     }
+
+    $pilaParentesis = [
+        
+    ];
+
+    $parejasParentesis = [
+            "(" => ")",
+            "{" => "}",
+            "[" => "]",
+    ];
+
+    function generarPilas(string $value) {
+
+        for($w = 0; $w <= strlen($value) - 1; $w++) {
+
+            //first level char
+            $char1 = $value[$w];
+
+            if (isset($parejasParentesis[$char1])) {
+                for($w2 = $w + 1; $w2 <= strlen($value) - 1; $w2++) {
+                    $char2 = $value[$w2];
+                    if ($char2 === $parejasParentesis[$char1]) {
+                        $newLevel = substr($value, $w, $w2 - $w);
+                        $pilaParentesis[] = $newLevel;
+                        
+                        
+                    }
+                }
+            }
+
+            
+
+        }
+        return true;
+    }
+
+    
 
     $result = validarParentesis($parentesis);
 
